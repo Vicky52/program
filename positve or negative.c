e549a6242d5cb220c871d36d191f39d5cbffdb53
Vicky52 <35257957+Vicky52@users.noreply.github.com>
2018-01-09 10:30:59 +0000

    Create positve or negative.c

---
+++ positve or negative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number;
+printf("enter the number");
+scanf(%d,&n);
+if(n>0)
+printf(%d is a positive number");
+else if(n<0)
+printf("%d is a negative number");
+else
+printf("%d is a zero");
+}
