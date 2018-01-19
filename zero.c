b3f0dabfa1709c4af40f45debb9c3b18f718cbd5
Vicky52 <35257957+Vicky52@users.noreply.github.com>
2018-01-19 16:09:51 +0530

    Create zero.c

---
+++ zero.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+void main()
+{
+printf("enter the number"\n)
+scanf(%d'&n);
+if(num>0)
+printf("it is a positive number");
+else if(num<0)
+printf(" it is a negative number");
+else
+printf("zero");
+}
