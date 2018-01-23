69b37e085d466abdff7fe7b1f37fb41eb01b356b
Vicky52 <35257957+Vicky52@users.noreply.github.com>
2018-01-23 10:08:24 +0000

    Create character.c

---
+++ character.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+int main()
+{
+char str[100];
+int i = 0, l = 0, f = 1;
+printf("Enter any string\n");
+scanf("%s",&str);
+if(str==1 || str==9)
+{
+l=l++;
+printf("the no of character in string is %d\n",l)
+getch();
+return 0;
+}
+
