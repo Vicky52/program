226f632009ac4ce6b72ce242944d346fd08a4e78
Vicky52 <35257957+Vicky52@users.noreply.github.com>
2018-01-25 11:24:36 +0000

    Create ra.c

---
+++ ra.c
@@ -0,0 +1,22 @@
+
+#include<>stdio.h>
+#incl;ude<conio.h>
+int main()
+{
+int n;
+char c[10];
+clrscr();
+printf("Enter the value \n",n);
+scanf("%d",&n);
+printf("enter the string:\n",c);
+scanf("%c",&c);
+if((n>=1)&&(n<=10))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
