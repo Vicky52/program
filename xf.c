8f17776a12870e99870063aa474e61f5cf840097
Vicky52 <35257957+Vicky52@users.noreply.github.com>
2018-02-01 14:57:48 +0530

    Create xf.c

---
+++ xf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[10];
+int e,i;
+printf(" enter the string:\n ");
+scanf("%s",&a);
+printf(" enter the number:\n ");
+scanf("%d",&e);
+for(i=1;i<=n;i++)
+{
+printf("%s\n",a);
+}
+return 0;
+}
