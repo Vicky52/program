584089944ebf118eb299a035f5179d5898b1d91e
Vicky52 <35257957+Vicky52@users.noreply.github.com>
2018-02-22 10:28:48 +0000

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char a[60];
+    int i,n;
+    scanf("%s",&a[i]);
+    if((n==0)||(n==1))
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+    }
