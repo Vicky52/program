1c21f7ea79b4e1e4b516fd4935b852f396b3aba0
Vicky52 <35257957+Vicky52@users.noreply.github.com>
2018-03-20 10:27:20 +0000

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	char a[10],i;
+	printf("enter the string:\n");
+
+	scanf("%s",&a[i]);
+	for(i=0;i<5;i++)
+	{
+	
+		if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+		{
+			printf("yes");
+			
+		}
+		else
+		{
+			printf("no");
+			
+		}
+	}
+	
+	return 0;
+}
